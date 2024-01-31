d8ef9e71b5b586df08128891ea2b932b22bdaf16
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2024-01-31 16:37:05 +0000

    Complete merge sort

---
+++ lib/merge_sort.rb
@@ -0,0 +1,17 @@
+def merge(arr)
+	return arr if arr.length < 2
+
+	mid = arr.length / 2
+
+	left = merge(arr[0...mid])
+	right = merge(arr[mid..-1])
+
+	sorted = []
+
+	until left.empty? || right.empty?
+    lowest = left.first < right.first ? left.shift : right.shift
+    sorted << lowest
+	end
+
+	(sorted << left << right).flatten
+end
